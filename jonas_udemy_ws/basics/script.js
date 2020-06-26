9fd2b6773b75f9a15e91c33fb42fa459c052b0ff
Praveen <praveen.babu36@gmail.com>
2020-06-26 19:54:50 +0530

    Javascript Variables Mutations and Operators

--- jonas_udemy_ws/basics/script.js
+++ jonas_udemy_ws/basics/script.js
@@ -1,1 +1,86 @@
+/****************************************
+ * Variables and data types
+ ***/
+/*
 console.log('Hello World !!!');
+
+var firstName = 'Praveen';
+
+console.log(firstName);
+
+
+var lastName = 'm';
+var age = 38;
+
+var fullAge = true;
+
+var job;
+
+var years$month;
+
+var boolean = 4.5;
+
+// values
+console.log(firstName, lastName, age, fullAge, job, boolean);
+
+//type 
+console.log(typeof(firstName), typeof(lastName), typeof(age),
+                typeof(fullAge), typeof(job));
+
+*/
+
+ /****************************************
+ * Variable Declaration and Type Coercion
+ ***/
+/*
+var firstName = 'Butter';
+var age = 21;
+var lastName = 'Fly';
+
+console.log(typeof(firstName) , typeof(age) );
+
+console.log(firstName + ' ' + age);
+
+console.log( typeof(firstName + ' ' + age) );
+
+var job, isMarried; 
+
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + 
+                '. Is he married ? ' + isMarried);
+
+*/
+
+ /*********************************************
+ * Variable mutation - change value of variable 
+ ***/              
+/*age = 'twenty one';
+job = 'driver'
+
+var lastName = prompt('what is his last name?');
+
+console.log(firstName + ' ' + lastName + ' is a ' + age + ' year(s) old ' + job + 
+                '. Is he married ? ' + isMarried);
+
+*/
+
+ /*********************************************
+ * Basic Operators 
+ ***/   
+
+var num1, num2, add, sub, mul, div;
+
+num1 = 25;
+num2 = 5;
+
+// Math
+console.log('Add:', num1 + num2);
+console.log('Sub:', num1 - num2);
+console.log('Mul:', num1 * num2);
+console.log('Div:', num1 / num2);
+
+//logical
+var num1Gt = num1 > num2;
+console.log(num1Gt, typeof num1Gt);
