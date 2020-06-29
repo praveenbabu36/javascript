a466cff7b8a29b99c8c3f547645f40eebfacf2a2
Praveen <praveen.babu36@gmail.com>
2020-06-30 03:27:54 +0530

    Conditional Operators, Truthy Falsy

--- jonas_udemy_ws/basics/script.js
+++ jonas_udemy_ws/basics/script.js
@@ -89,7 +89,7 @@ console.log(num1Gt, typeof num1Gt);
  /*********************************************
  * Operator Precedence
  ***/   
-
+/*
 var res = 3 + 4 * 5;
 
 console.log(res);
@@ -128,4 +128,121 @@ var d = 2;
 
 d++;
 
-console.log(d);
+console.log(d);*/
+
+
+ /*********************************************
+ * Coding Challenge - 1
+ * Compare body mass index of mark and john. 
+ *  Print whether BMI of Mark is higher than John.
+ *  BMI = Mass / Height ^ 2
+ ***/   
+/*
+ var john_mass = 100;
+ var john_height = 2.5;
+
+ var mark_mass = 90;
+ var mark_height = 2.2;
+
+var john_bmi = john_mass / (john_height * john_height);
+var mark_bmi = mark_mass / (mark_height * mark_height);
+
+var bmiFlag = mark_bmi > john_bmi;
+
+console.log('Is BMI of Mark greater than John: ', bmiFlag);
+*/
+
+ /*********************************************
+ * If Else Statements  
+ ****/
+/*
+var firstName = 'John';
+var civilStatus = 'Single';
+
+if(civilStatus === 'Married') { 
+    console.log(firstName + ' is married.');
+} else {
+    console.log(firstName + ' is Single.');
+}
+
+// using a boolean
+var isMarried = true;
+
+if(isMarried) { 
+    console.log(firstName + ' is married.');
+} else {
+    console.log(firstName + ' is Single.');
+}
+*/
+
+ /*********************************************
+ * Boolean Logic - If Else Statements  
+ ****/
+/*
+var firstName = 'John';
+var age = 20;
+
+if( age < 13 ) {
+    console.log(firstName + ' is a boy.' );
+} else if(age >= 13 && age < 20 ) {
+    console.log(firstName + ' is a teenager');
+} else if(age >= 20 && age <= 30 ) {
+    console.log(firstName + ' is a young man');
+} else {
+    console.log(firstName + ' is a man')
+}
+*/
+
+ /*********************************************
+ * Ternary Operator and Switch Statements
+ ****/
+/*
+ var firstName = 'John';
+ var age = 19;
+
+//ternary operator
+var drink = age >= 18 ? 'Beer' : 'Juice';
+
+console.log(firstName + ' drinks ' + drink );
+*/
+
+ /*********************************************
+ * Switch Statements
+ ****/
+/*
+var firstName = 'John';
+var job = 'Teacher';
+
+switch(job) {
+    case 'Teacher'  : console.log(firstName + ' teaches how to code.');
+                      break;
+
+    case 'Driver'   : console.log(firstName + ' drives an uber in Chennai.');
+                      break;
+
+    case 'Designer' : console.log(firstName + ' designs beautiful websites.');
+                      break;
+
+    default         : console.log(firstName + ' does something else.');
+}
+*/
+
+ /*********************************************
+ * Truthy & Falsy
+ ****/
+// falsy :  0, '', null, undefined, NAN
+// truthy : not falsy
+
+var height = '23';
+
+if(height || height === 0 ) { // if true
+    console.log('Variable height is defined.');
+}else{
+    console.log('Variable height has not been defined.');
+}
+
+if(height === '23') {
+    console.log('The === operator does not do Type Coercion');
+} else if(height == '23') {
+    console.log('The == operator does Type Coercion');
+} 
